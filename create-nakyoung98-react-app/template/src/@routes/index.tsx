cc83f64c1112af097f9f715be16711d70242c3b6
nakyoung98 <jay3925@cau.ac.kr>
2024-11-07 17:20:55 +0900

    Feat: react-router 설정

--- create-nakyoung98-react-app/template/src/@routes/index.tsx
+++ create-nakyoung98-react-app/template/src/@routes/index.tsx
@@ -1,12 +1,24 @@
 /**
- * 
+ *
  * @routes: 라우팅 설정을 관리하는 폴더입니다.
- * 
+ *
  * [index.tsx]
- * 
+ *
  * 라우트 설정을 관리합니다.
  * - 라우트 정의
  * - 라우트 설정
  * - 중첩 라우팅
- * 
+ *
  */
+
+import Home from "@pages/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+export default Router;
--- create-nakyoung98-react-app/template/src/App.tsx
+++ create-nakyoung98-react-app/template/src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import Router from "@routes";
+
 function App() {
-  return <></>;
+  return <RouterProvider router={Router} />;
 }
 
-export default App
+export default App;
