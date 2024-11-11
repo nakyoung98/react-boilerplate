fce6b89d5995b63eddbb9b4d6fee785a7256aca7
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-11-11 11:11:54 +0000

    Library: react-query-devtools 설치

--- create-nakyoung98-react-app/template/src/App.tsx
+++ create-nakyoung98-react-app/template/src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import Router from "@routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,6 +14,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <RouterProvider router={Router} />
     </QueryClientProvider>
   );
