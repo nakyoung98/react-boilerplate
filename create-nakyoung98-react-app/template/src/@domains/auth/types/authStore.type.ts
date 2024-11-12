9acf84e5984d9b1222933fe327794766c4dd4280
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-11-12 06:03:56 +0000

    Feat: AuthStore 타입 및 jwt util 구현

---
+++ create-nakyoung98-react-app/template/src/@domains/auth/types/authStore.type.ts
@@ -0,0 +1,15 @@
+export type AuthState = {
+  accessToken: string | null;
+  tokenPayload: TokenPayload | null;
+};
+
+export type AuthActions = {
+  setAccessToken: (accessToken: string) => void;
+  setTokenPayload: (accessToken: string) => void;
+};
+
+export type TokenPayload = {
+  sub: string;
+  exp: number;
+  lat: number;
+};
---
+++ create-nakyoung98-react-app/template/src/@domains/auth/utils/jwt.util.ts
@@ -0,0 +1,16 @@
+export default class JWTUtils {
+  static getPayload = (jwt: string) => {
+    const rawPayload = jwt.split(".")[1].replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      window
+        .atob(rawPayload)
+        .split("")
+        .map(
+          (letter) => "%" + ("00" + letter.charCodeAt(0).toString(16)).slice(-2)
+        )
+        .join("")
+    );
+
+    return JSON.parse(jsonPayload)
+  };
+}
