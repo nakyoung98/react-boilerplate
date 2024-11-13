c37d66da89dd98f703f17269a81f4cbe9c0fa1b2
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-11-13 08:33:31 +0000

    Library: vitest 설치

--- create-nakyoung98-react-app/template/vite.config.ts
+++ create-nakyoung98-react-app/template/vite.config.ts
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@commons': path.resolve(__dirname, './src/@commons'),
-      '@config': path.resolve(__dirname, './src/@config'),
-      '@domains': path.resolve(__dirname, './src/@domains'),
-      '@lib': path.resolve(__dirname, './src/@lib'),
-      '@mocks': path.resolve(__dirname, './src/@mocks'),
-      '@network': path.resolve(__dirname, './src/@network'),
-      '@pages': path.resolve(__dirname, './src/@pages'),
-      '@routes': path.resolve(__dirname, './src/@routes')
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+      "@commons": path.resolve(__dirname, "./src/@commons"),
+      "@config": path.resolve(__dirname, "./src/@config"),
+      "@domains": path.resolve(__dirname, "./src/@domains"),
+      "@lib": path.resolve(__dirname, "./src/@lib"),
+      "@mocks": path.resolve(__dirname, "./src/@mocks"),
+      "@network": path.resolve(__dirname, "./src/@network"),
+      "@pages": path.resolve(__dirname, "./src/@pages"),
+      "@routes": path.resolve(__dirname, "./src/@routes"),
+    },
+  },
+
+  test: {
+    environment: "happy-dom",
+  },
+});
