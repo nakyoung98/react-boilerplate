c81bb5c9e3dbeb4c7295186683ed9105c0baf8ca
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-12-02 07:52:49 +0000

    Feat: auth api - signin 구현

---
+++ create-nakyoung98-react-app/template/src/@network/apis/auth.api.ts
@@ -0,0 +1,12 @@
+import { ApiService } from "@network/services/apiService";
+import { SigninResponseDTO, SigninResquestDTO } from "@network/types/dtos/auth.dto";
+
+export const AuthAPI = {
+  signin: async (params: SigninResquestDTO) => {
+    await ApiService.mutateData<SigninResquestDTO, SigninResponseDTO>({
+      endpoint: "/signin",
+      method: "POST",
+      data: params,
+    });
+  },
+};
---
+++ create-nakyoung98-react-app/template/src/@network/types/dtos/auth.dto.ts
@@ -0,0 +1,8 @@
+export type SigninResquestDTO = {
+  id: string;
+  password: string;
+};
+
+export type SigninResponseDTO = {
+  accessToken: string;
+};
