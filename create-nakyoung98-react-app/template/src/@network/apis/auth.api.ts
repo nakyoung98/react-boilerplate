ad0bf2d3c17e51de9575636cdc7a2876bb7a277e
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-12-11 01:53:35 +0000

    Fix: AuthAPI signin return 오류 수정

--- create-nakyoung98-react-app/template/src/@network/apis/auth.api.ts
+++ create-nakyoung98-react-app/template/src/@network/apis/auth.api.ts
@@ -2,11 +2,10 @@ import { ApiService } from "@network/services/apiService";
 import { SigninResponseDTO, SigninResquestDTO } from "@network/types/dtos/auth.dto";
 
 export const AuthAPI = {
-  signin: async (params: SigninResquestDTO) => {
+  signin: async (params: SigninResquestDTO) =>
     await ApiService.mutateData<SigninResquestDTO, SigninResponseDTO>({
       endpoint: "/signin",
       method: "POST",
       data: params,
-    });
-  },
+    }),
 };
