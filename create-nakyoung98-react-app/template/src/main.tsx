6f03dc63dc432c0db0a1cd418a5067f091aa6f02
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-12-12 15:25:00 +0000

    Chore: 불필요 주석 제거

--- create-nakyoung98-react-app/template/src/main.tsx
+++ create-nakyoung98-react-app/template/src/main.tsx
@@ -10,8 +10,6 @@ async function enableMocking() {
 
   const { worker } = await import("@mocks/browser.ts");
 
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
   return worker.start();
 }
 
