a99ed7046cbe2591a5a5d453a5f2645a18337311
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-12-27 05:06:00 +0000

    Feat: AccessToken 만료 시 refresh 먼저 시도하도록 구현

--- create-nakyoung98-react-app/template/src/@network/interceptors/request.interceptor.ts
+++ create-nakyoung98-react-app/template/src/@network/interceptors/request.interceptor.ts
@@ -1,11 +1,42 @@
+import { AuthService } from "@domains/auth/services/authService";
 import { useAuthStore } from "@domains/auth/stores/authStore";
+import JWTUtils from "@domains/auth/utils/jwt.util";
+import { AuthAPI } from "@network/apis/auth.api";
 import { InternalAxiosRequestConfig } from "axios";
 
-export const addAuthTokenInterceptor = (config: InternalAxiosRequestConfig<any>) => {
-  const accessToken = useAuthStore.getState().accessToken;
-  if (typeof accessToken == "string" && accessToken.trim() !== "") {
-    config.headers.Authorization = accessToken;
+export const addAuthTokenInterceptor = async (config: InternalAxiosRequestConfig<any>) => {
+  //토큰 갱신 요청 시에는 인터셉터 무시
+  if(config.url?.includes(AuthAPI.REFRESH)){
+    return config;
   }
-  return config;
-};
 
+  const accessToken = useAuthStore.getState().accessToken;
+
+  // AccessToken 존재
+  if (typeof accessToken == "string" && accessToken.trim() !== "") {
+    // token 유효성 검사
+    const tokenPayload = useAuthStore.getState().tokenPayload;
+    if (tokenPayload && JWTUtils.isExpValid(tokenPayload)) {
+      config.headers.Authorization = accessToken;
+      return config;
+    }
+  }
+
+  // AccessToken 미존재(RefreshToken은 HttpOnly로 접근할 수 없으므로 바로 refresh 요청 보냄)
+  try {
+    const {
+      data: { accessToken: newAccessToken },
+    } = await AuthService.refresh();
+    const setAccessToken = useAuthStore.getState().setAccessToken;
+
+    if (!newAccessToken) {
+      return Promise.reject(new Error());
+    }
+
+    setAccessToken(newAccessToken);
+    config.headers.Authorization = newAccessToken;
+    return config;
+  } catch (error: any) {
+    return Promise.reject(error);
+  }
+};
